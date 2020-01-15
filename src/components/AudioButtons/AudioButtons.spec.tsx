015080a501dd0e322e58392ede21cb63bef7fb49
Yodra <yodra@leanmind.es>
2020-01-15 14:32:56 +0000

    add test fix and refactor

--- src/components/AudioButtons/AudioButtons.spec.tsx
+++ src/components/AudioButtons/AudioButtons.spec.tsx
@@ -1,23 +1,27 @@
 import * as React from 'react';
-import { render, RenderResult } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import { AudioButtons } from './';
 import { AudioService } from '../../services/AudioService';
 
+const renderAudioButton = () => {
+  const utils = render(<AudioButtons audioService={new AudioService()}/>);
+  const buttonStart = utils.getByLabelText('start');
+  const buttonStop = utils.getByLabelText('stop');
+  return {buttonStart, buttonStop, ...utils};
+};
+
 describe('AudioButtons', () => {
-  it('should be render a start button and a stop button', () => {
-    const { getByLabelText } = render(<AudioButtons audioService={new AudioService()}/>);
-    const buttonStart = getByLabelText(/start/);
-    const buttonStop = getByLabelText(/stop/);
+  it('should render a start button and a stop button', () => {
+    const { buttonStart, buttonStop } = renderAudioButton();
 
     expect(buttonStart).toBeDefined();
     expect(buttonStop).toBeDefined();
   });
 
-  it('should be render a start button and a stop button', () => {
-    const { getByLabelText, getByRole } = render(<AudioButtons audioService={new AudioService()}/>);
-    const buttonStart = getByLabelText(/start/);
-    const buttonStop = getByRole('button');
+  it('should render a enabled start button and a disabled stop button', () => {
+    const { buttonStart, buttonStop } = renderAudioButton();
 
+    expect(buttonStart).not.toHaveAttribute('disabled');
     expect(buttonStop).toHaveAttribute('disabled');
   });
 });
