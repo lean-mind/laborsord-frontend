8adc973bd890178b5798becdfb819af4ebad8a50
Adrian Ferrera <adrian.afergon@gmail.com>
2020-01-16 22:25:01 +0000

    Clone type props from ReactDiv to Container

--- src/components/Container/Container.tsx
+++ src/components/Container/Container.tsx
@@ -1,11 +1,9 @@
 import * as React from 'react';
 import './Container.scss';
 
-interface Props {
-  className?: string;
-}
+type ContainerProps = React.DetailedHTMLProps<React.HTMLAttributes<HTMLDivElement>, HTMLDivElement>
 
-export const Container: React.FC<Props> = ({ className, children, ...props }) => (
+export const Container: React.FC<ContainerProps> = ({ className, children, ...props }) => (
   <div className={className} {...props}>
     {children}
   </div>
