d37e3123c175e1a6c64c871c5eb3f443e17fec8b
Yodra <yodra@leanmind.es>
2020-01-27 10:28:24 +0000

    add logic to button start and stop in Speech

--- src/components/Speech/Speech.tsx
+++ src/components/Speech/Speech.tsx
@@ -12,7 +12,7 @@ recognition.continous = true;
 recognition.interimResults = true;
 recognition.lang = 'es-ES';
 
-export const Speech: FC<{}> = () => {
+export const Speech: FC = () => {
   const [isListening, setIsListening] = useState(false);
   const [finalText, setFinalText] = useState('');
   const [interimText, setInterimText] = useState('');
@@ -22,66 +22,69 @@ export const Speech: FC<{}> = () => {
   };
 
   useEffect(() => {
-    // const handleListen = () => {
-      if (isListening) {
+    if (isListening) {
+      recognition.start();
+      recognition.onend = () => {
+        console.log('...continue listening...');
         recognition.start();
-        recognition.onend = () => {
-          console.log('...continue listening...');
-          recognition.start();
-        };
-      } else {
-        recognition.stop();
-        recognition.onend = () => {
-          console.log('Stopped listening per click');
-        };
+      };
+    } else {
+      recognition.stop();
+      recognition.onend = () => {
+        console.log('Stopped listening per click');
+      };
+    }
+
+    recognition.onstart = () => {
+      console.log('Listening!');
+    };
+
+    let finalTranscript = '';
+    recognition.onresult = (event: any) => {
+      let interimTranscript = '';
+
+      for (let i = event.resultIndex; i < event.results.length; i++) {
+        const transcript = event.results[i][0].transcript;
+        if (event.results[i].isFinal) {
+          finalTranscript += transcript + ' ';
+        } else {
+          interimTranscript += transcript;
+        }
       }
 
-      recognition.onstart = () => {
-        console.log('Listening!');
-      };
+      setInterimText(interimTranscript);
+      setFinalText(finalTranscript);
 
-      let finalTranscript = '';
-      recognition.onresult = (event: any) => {
-        let interimTranscript = '';
+      // -------------------------COMMANDS------------------------------------
+      const transcriptArr = finalTranscript.split(' ');
+      const stopCmd = transcriptArr.slice(-3, -1);
+      console.log('stopCmd', stopCmd);
 
-        for (let i = event.resultIndex; i < event.results.length; i++) {
-          const transcript = event.results[i][0].transcript;
-          if (event.results[i].isFinal) {
-            finalTranscript += transcript + ' ';
-          } else {
-            interimTranscript += transcript;
-          }
-        }
+      if (stopCmd[0] === 'stop' && stopCmd[1] === 'listening') {
+        recognition.stop();
+        recognition.onend = () => {
+          console.log('Stopped listening per command');
+          setFinalText(transcriptArr.slice(0, -3).join(' '));
+        };
+      }
+    };
+    // -----------------------------------------------------------------------
 
-        setInterimText(interimTranscript);
-        setFinalText(finalTranscript);
-
-        // -------------------------COMMANDS------------------------------------
-        const transcriptArr = finalTranscript.split(' ');
-        const stopCmd = transcriptArr.slice(-3, -1);
-        console.log('stopCmd', stopCmd);
-
-        if (stopCmd[0] === 'stop' && stopCmd[1] === 'listening') {
-          recognition.stop();
-          recognition.onend = () => {
-            console.log('Stopped listening per command');
-            setFinalText(transcriptArr.slice(0, -3).join(' '));
-          };
-        }
-      };
-      // -----------------------------------------------------------------------
-
-      recognition.onerror = (event: any) => {
-        console.log('Error occurred in recognition: ' + event.error);
-      };
+    recognition.onerror = (event: any) => {
+      console.log('Error occurred in recognition: ' + event.error);
+    };
   }, [isListening]);
 
+  const nameButton = isListening ? 'Stop' : 'Start';
   return (
     <Container className="Speech">
-      <Button className="start" ariaLabel="Start" onClick={toggleListen}>Start</Button>
+      <Button className={isListening ? 'stop' : 'start'} ariaLabel={nameButton} onClick={toggleListen}>{nameButton}</Button>
       {/*<Button className="stop" ariaLabel="Stop">Stop</Button>*/}
-      <div id="final">{finalText}</div>
-      <div id="interim">{interimText}</div>
+      <Container className="Transcription">
+        {!finalText && !interimText && 'Esperando a que empiece la clase...'}
+        {finalText}
+        {interimText}
+      </Container>
     </Container>
   );
 };
